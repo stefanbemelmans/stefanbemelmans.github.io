53a3bcb255bfb1f6dc2465e93e94d2c5879af1cd
stackbaxter <stackbaxter@yahoo.com>
2017-07-24 15:10:55 -0500

    fixing it

---
+++ catFight-app/Bio.js
@@ -0,0 +1,14 @@
+ function Bio (props){
+    return(
+        <div className='bio'>
+            <img src='./images/catForkTrans.png' />
+            <div className='profileText'>
+                wordsy wordsy wordsy
+                </div>
+        </div>
+
+
+
+
+    )
+}
