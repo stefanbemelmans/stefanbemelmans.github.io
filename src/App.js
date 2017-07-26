737eb7d562cc08fa0ead3810ca484e57b1c54de0
stackbaxter <stackbaxter@gmail.com>
2017-07-26 12:13:54 -0500

    the Beginning Again

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import App from 'App';
 import Header from './components'
 
-function App(props){
+export default function App(props){
   return(
   
   <Header />
 
-)
+  )
 
 }
