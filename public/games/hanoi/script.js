e5b8bb1ca606bbc5ecba7edc9e3b1eadd1d2f3aa
stackbaxter <stackbaxter@yahoo.com>
2017-10-14 02:32:17 -0500

    got the background outta there, to confusing

---
+++ public/games/hanoi/script.js
@@ -0,0 +1,29 @@
+$(function() {
+  // Put app logic here
+  // let $ringVal = $(this).children().last();
+  let $ring = 'null';
+  let length = $(this).children().last().length;
+
+  $('[data-stack]').click(function(){
+
+      if($ring === 'null'){
+        $ring = $(this).children().last().detach();
+        console.log($ring);
+        console.log($(this).children().length);
+        }
+
+      else if ($(this).children().length === 0 || ($ring.data('block') < $(this).children().last().data('block') ))
+       {
+         $(this).append($ring);
+         $ring = 'null';
+         checkWin();
+       }
+
+     })
+
+function checkWin(){
+  if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length===4) {
+    $('#announce-game-won').text('You\'ve Won!');
+  }
+}
+  });
